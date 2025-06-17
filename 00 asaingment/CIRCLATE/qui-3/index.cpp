5605d228d3415ecae1be1706396df19ce385de4e
Dharmik Ahir <sangadharmik78@gmail.com>
2025-06-17 17:45:46 +0530

    assaignment

--- 00 asaingment/CIRCLATE/qui-3/index.cpp
+++ 00 asaingment/CIRCLATE/qui-3/index.cpp
@@ -1,23 +1,22 @@
-#include <iostream> 
+#include<iostream>
+
 using namespace std;
 
-int main() {
+int main(){
+     
+    for(int i=5;i>=1;i--){
 
-
-        for (int i=5; i>=1; i--)
+        for(int k=1; k<i; k++)
         {
-             
-            for (int k=1; k<i; k++)
-            {
-                cout << " "<< " " << endl;
-            }
-
-            for (int j=i; j<=5; j++)
-            {
-                cout << j <<" ";
-            }
-            cout << endl;
+            cout << " " << " ";
         }
-        return 0;
 
+        for(int j=i; j<=5; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
 }
