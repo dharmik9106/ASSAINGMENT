ac1e212fc9165d8029e4f9214fc795323ada4c4b
Dharmik Ahir <sangadharmik78@gmail.com>
2025-06-17 17:48:08 +0530

    circlet complate

--- 00 asaingment/CIRCLATE/qui-1/index.cpp
+++ 00 asaingment/CIRCLATE/qui-1/index.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    for (int i=20; i<=40; i++)
-    {
-        for (int j=50;j<=i;j++)
-        {
-
-            cout << j <<" ";
+int main(){
+     
+    for(int i=30; i<=50; i++){
+        
+        for(int j=30;j<=i;j++){
+            
+            cout << j << " ";
         }
-
         cout << endl;
     }
-        return 0;
+    return 0;
 }
